726e4e2d2b0eadab2e50de656d08ac831fd2f206
PawelDrwiega <161934902+kolec23@users.noreply.github.com>
2025-10-06 18:47:29 +0200

    Update int128.cpp
    Add functions  verfication datas.
    Dodaj funkcje weryfikacji danych.

--- int128.cpp
+++ int128.cpp
@@ -11,16 +11,18 @@ int128::int128(__int128 value)
 	this->wartosc=value;
 }
 
-int128::int128(std::string number) //dorobic sprawdzenie poprawnosci 
+int128::int128(std::string number) 
 {
 	
 	__int128 waga=1;
 	__int128 cyfra;
 	__int128 wynik=0;
+	if(this->sprawdzCzyLiczba(number[0])==false) throw std::invalid_argument("That isn't number'");
 	if(number[0]=='-')
 	{
 		for(int i=number.size()-1; i>0; i--)
 		{
+			if(this->sprawdzCzyLiczba(number[i])==false) throw std::invalid_argument("That isn't number'");
 			cyfra=number[i]-'0';
 			wynik=wynik+cyfra*waga;
 			waga=waga*10;
@@ -31,7 +33,7 @@ int128::int128(std::string number) //dorobic sprawdzenie poprawnosci
 	{
 		
 		for(int i=number.size()-1; i>=0; i--)
-		{	
+		{	if(this->sprawdzCzyLiczba(number[i])==false) throw std::invalid_argument("That isn't number'");
 			cyfra=number[i]-'0';
 			wynik=wynik+cyfra*waga;
 			waga=waga*10;
@@ -72,6 +74,7 @@ int128 int128::operator% (int128 b)
 
 int128 int128::operator/ (int128 b)
 {
+	if(b.wartosc==0) throw std::invalid_argument("Divide by zero don't allowed.");
 	__int128 pom=this->wartosc/b.wartosc;
 	int128 wynik(pom);
 	return wynik;
@@ -168,5 +171,24 @@ std::string int128::konwertuj()
 		wynik="-"+wynik;
 	}
 	return wynik;
+}
+
+bool int128::sprawdzCzyLiczba( char z)
+{
+	switch(z)
+	{
+		case '-': return true; break;
+		case '0': return true; break;
+		case '1': return true; break;
+		case '2': return true; break;
+		case '3': return true; break;
+		case '4': return true; break;
+		case '5': return true; break;
+		case '6': return true; break;
+		case '7': return true; break;
+		case '8': return true; break;
+		case '9': return true; break;
+		default: return false;
+	}
 }
 
